98a8648739900047e5ab680ad834988c63763f81
Ddaauto <ddaautoparts@gmail.com>
2026-04-03 11:54:49 -0400

    Delete cancel.jsx

--- frontend/src/cancel.jsx
+++
@@ -1,8 +0,0 @@
-export default function Cancel() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>❌ Payment Canceled</h1>
-      <p>No charge was made. You can try again.</p>
-    </div>
-  );
-}
